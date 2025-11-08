06474b7c2b4b8ffe800423d6605b754d51da6c13
Andrew Cherry <andrew@xyncro.com>
2025-11-08 11:13:21 +0000

    starting to experiment with actual impl of dcb-like approach

--- examples/eventric-surface-examples/examples/course_manager.rs
+++ examples/eventric-surface-examples/examples/course_manager.rs
@@ -1,5 +1,17 @@
 use std::error::Error;
 
+use derive_more::Debug;
+use fancy_constructor::new;
+
+#[derive(new, Debug)]
+pub struct CourseRegistered {
+    #[new(into)]
+    pub id: String,
+    #[new(into)]
+    pub title: String,
+    pub capacity: u8,
+}
+
 pub fn main() -> Result<(), Box<dyn Error>> {
     println!("course manager...");
 
